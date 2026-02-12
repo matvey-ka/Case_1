7aae74d8874ac9f9332a501950d3f1856f3e4987
Matvey Popov <m.popov6@g.nsu.ru>
2026-02-12 16:46:33 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import turtle
+
+
+def square(x, y, a, color):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.end_fill()
+
+
+def triangle(x, y, a, color):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.left(120)
+    turtle.forward(a)
+    turtle.right(120)
+    turtle.end_fill()
+
+
+def rectangle(x, y, a, b, color):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(b)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(b)
+    turtle.right(90)
+    turtle.end_fill()
+
+
+def circle(x, y, r, color):
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+    turtle.circle(r)
+    turtle.end_fill()
+
+
+def main():
+    square(-200, -100, 200, 'red')
+    triangle(100, -300, 200, 'blue')
+    rectangle(-200, 200, 200, 100, 'green')
+    circle(200, 50, 100, 'yellow')
+    turtle.done()
+
+
+main()
